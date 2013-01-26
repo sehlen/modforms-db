0e98bf08e754ce72027774886ea49a95448cfaf4
Stephan Ehlen <stephan.j.ehlen@gmail.com>
2013-01-26 23:17:22 +0000

    Implementing the new naming scheme and adding geter/setter for preperties (automatically via a new metaclass).

---
+++ mdb/accessor.py
@@ -0,0 +1,50 @@
+import bz2 as comp
+from elixir import EntityMeta
+
+def _addMethod(fldName, clsName, verb, methodMaker, dict):
+    """Make a get or set method and add it to dict."""
+    compiledName = _getCompiledName(fldName, clsName)
+    methodName = _getMethodName(fldName, verb)
+    dict[methodName] = methodMaker(compiledName)
+    
+def _getCompiledName(fldName, clsName):
+    """Return mangled fldName if necessary, else no change."""
+    # If fldName starts with 2 underscores and does *not* end with 2 underscores...
+    if fldName[:2] == '__' and fldName[-2:] != '__':
+        return "_%s%s" % (clsName, fldName)
+    else:
+        return fldName
+
+def _getMethodName(fldName, verb):
+    """'_salary', 'get'  => 'get_salary'"""
+    s = fldName.lstrip('_') # Remove leading underscores
+    return verb + '_' + s
+
+def _makeGetterCompressed(compiledName):
+    """Return a method that gets compiledName's value."""
+    return lambda self: comp.decompress(str(self.__dict__[compiledName]))
+
+def _makeSetterCompressed(compiledName):
+    """Return a method that sets compiledName's value."""    
+    return lambda self, value: setattr(self, compiledName, comp.compress(str(value)))
+
+def _makeGetter(compiledName):
+    """Return a method that gets compiledName's value."""
+    return lambda self: self.__dict__[compiledName]
+
+def _makeSetter(compiledName):
+    """Return a method that sets compiledName's value."""    
+    return lambda self, value: setattr(self, compiledName, value)
+
+class Accessors(EntityMeta):
+    """Adds accessor methods to a class."""
+    def __new__(cls, clsName, bases, dict):
+        for fldName in dict.get('_READ', []) + dict.get('_READ_WRITE', []):
+            _addMethod(fldName, clsName, 'get', _makeGetter, dict)
+        for fldName in dict.get('_WRITE', []) + dict.get('_READ_WRITE', []):
+            _addMethod(fldName, clsName, 'set', _makeSetter, dict)
+        for fldName in dict.get('_COMPRESSED',[]):
+            print 'adding get and set for ', fldName
+            _addMethod(fldName, clsName, 'set', _makeSetterCompressed, dict)
+            _addMethod(fldName, clsName, 'get', _makeGetterCompressed, dict)
+        return EntityMeta.__new__(cls, clsName, bases, dict)
--- mdb/schema.py
+++ mdb/schema.py
@@ -2,7 +2,7 @@ import inspect
 import os
 from elixir import *
 from sqlalchemy.ext.associationproxy import AssociationProxy
-import bz2 as comp
+from accessor import Accessors
 
 prefix='schema.'
 f = inspect.getabsfile(inspect.currentframe())
@@ -11,7 +11,11 @@ datadir = "/".join(os.path.dirname(f).rsplit("/")[0:-1]+["data"])
 metadata.bind = "sqlite:///{0}/modularforms.sqlite".format(datadir)
 metadata.bind.echo = True
 
+compress = True
+
 class ModularSymbols_ambient_DB(Entity):
+    __metaclass__ = Accessors
+    
     # primary key
     level = Field(Integer, primary_key=True)
     weight = Field(Integer, primary_key=True)
@@ -20,10 +24,19 @@ class ModularSymbols_ambient_DB(Entity):
     
     # data to reconstruct the space
     # TODO: add documentation!
-    basis = Field(Binary)
-    manin = Field(Binary)
-    rels = Field(Binary)
-    mod2term = Field(Binary)
+    bfields = ['_basis', '_manin', '_rels', '_mod2term']
+    if compress:
+        _basis = Field(Binary, colname = 'basis')
+        _manin = Field(Binary, colname = 'manin')
+        _rels = Field(Binary, colname = 'rels')
+        _mod2term = Field(Binary, colname = 'mod2term')
+        _COMPRESSED = bfields
+    else:
+        _basis = Field(Text, colname = 'basis')
+        _manin = Field(Text, colname = 'manin')
+        _rels = Field(Text, colname = 'rels')
+        _mod2term = Field(Text, colname = 'mod2term')
+        _READ_WRITE = bfields
 
     # the field of values of the character
     has_one('base_field', of_kind='{0}ModularSymbols_base_field_DB'.format(prefix))
@@ -59,47 +72,21 @@ class ModularSymbols_oldspace_factor_DB(Entity):
 class ModularSymbols_newspace_factor_DB(Entity):
     belongs_to('ambient', of_kind='{0}ModularSymbols_ambient_DB'.format(prefix))
     # data to rectonstruct the ModularSymbols space
-    _B = Field(Binary, colname = 'B')
-    _Bd = Field(Binary, colname = 'Bd')
-    _v = Field(Binary, colname = 'v')
-    _nz = Field(Binary, colname = 'nz')
-    #
-    @property
-    def B(self):
-        if self._B is not None:
-            return comp.decompress(str(self._B))
-        
-    def set_B(self, B):
-        if B is not None:
-            self._B = comp.compress(str(B))
 
-    @property
-    def Bd(self):
-        if self._Bd is not None:
-            return comp.decompress(str(self._Bd))
-        
-    def set_Bd(self, Bd):
-        if Bd is not None:
-            self._Bd = comp.compress(str(Bd))
+    bfields = ['_B', '_Bd', '_v', '_nz']
+    if compress:
+        _B = Field(Binary, colname = 'B')
+        _Bd = Field(Binary, colname = 'Bd')
+        _v = Field(Binary, colname = 'v')
+        _nz = Field(Binary, colname = 'nz')
+        _COMPRESSED = bfields
+    else:
+        _B = Field(Text, colname = 'B')
+        _Bd = Field(Text, colname = 'Bd')
+        _v = Field(Text, colname = 'v')
+        _nz = Field(Text, colname = 'nz')
+        _READ_WRITE = bfields
 
-    @property
-    def v(self):
-        if self._v is not None:
-            return comp.decompress(str(self._v))
-        
-    def set_v(self, v):
-        if v is not None:
-            self._v = comp.compress(str(v))
-
-    @property
-    def nz(self):
-        if self._nz is not None:
-            return comp.decompress(str(self._nz))
-        
-    def set_nz(self, nz):
-        if nz is not None:
-            self._nz = comp.compress(str(nz))
-    #
     dimension = Field(Integer)
     has_cm = Field(Boolean) # has complex multiplication?
     has_one('coefficient_field', of_kind='{0}CoefficientField_DB'.format(prefix))
@@ -136,17 +123,14 @@ class AlgebraicNumber_DB(Entity):
     belongs_to('coefficient', of_kind='{0}Coefficient_DB'.format(prefix))
 
     # _value is the coefficient vector in terms of a power basis
-    # the 
-    # it is stored with bz2 compression
-    _value = Field(Binary, colname='value')
-
-    @property
-    def value(self, v=None):
-        if v is not None:
-            self._value = comp.compress(str(v))
-        else:
-            if self._value is not None:
-                return comp.decompress(str(self._value))
+    # of the number field (with specified minimal polynomial)
+    bfields = ['_value']
+    if compress:
+        _value = Field(Binary, colname='value')
+        _COMPRESSED = bfields
+    else:
+        _value = Field(Text, colname='value')
+        _READ_WRITE = bfields
     
     def __repr__(self):                    
         return 'Algebraic Number {0}, element of Number Field with defining polynomial {1} over its base field.'.format(
