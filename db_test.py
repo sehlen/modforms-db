01544a372d54072f15d22f5ca9c68e8187b0d726
Fredrik Stroemberg <fredrik314@gmail.com>
2013-09-12 00:34:48 +0100

    Fixed weight 1 problem

--- db_test.py
+++ db_test.py
@@ -901,18 +901,22 @@ def generate_dimension_table_gamma0(DB,maxN=100, maxk=12,db='to'):
             dim = dimension_new_cusp_forms(N, k)
             finds = ms.find({'N':int(N),'k':int(k),'chi':int(0)})
                 #{'t': [int(N), int(k), int(0)]})
-            print finds.count()
+            #print finds.count()
             in_db = finds.count() > 0
             data[N][k] = {'dimension': dim, 'in_db': in_db}
             # print N,k,data[N][k]
     return ms, data
 
 
-def generate_dimension_table_gamma1(maxN=100, maxk=12, minN=3, mink=2):
-    C = pymongo.connection.Connection(port=dbport)
-    C = pymongo.connection.Connection(port=dbport)
-    ms = C['modularforms']['Modular_symbols.files']
-    print ms
+def generate_dimension_table_gamma1(DB,maxN=100, maxk=12, minN=3, mink=2,db='to'):
+    #C = pymongo.connection.Connection(port=dbport)
+    #C = pymongo.connection.Connection(port=dbport)
+    #ms = C['modularforms']['Modular_symbols.files']
+    if db=='to':        
+        ms = DB._ms_collection_to.files # = C['modularforms']['Modular_symbols.files']
+    elif db=='fr':
+        ms = DB._ms_collection_fr.files # = C['modularforms']['Modular_symbols.files']
+        print ms
     data = dict()
     for N in range(minN, maxN + 1):
         data[N] = dict()
@@ -946,3 +950,14 @@ def generate_dimension_table_gamma1(maxN=100, maxk=12, minN=3, mink=2):
             data[N][k][-1] = {'dimension': dimall, 'in_db': in_db_all}
         print "Computed data for level ", N
     return ms, data
+
+
+def generate_dimension_table(DB):
+    r0,d0 = generate_dimension_table_gamma0(DB)
+    r1,d1 = generate_dimension_table_gamma1(DB)
+    res = {'group':'gamma0',data=bson.binary.Binary(d0)}
+    DB._mongod_to.dimensions.insert(res)
+    res = {'group':'gamma1',data=bson.binary.Binary(d1)}
+    DB._mongod_to.dimensions.insert(res)
+    return 
+    
