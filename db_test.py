777abc6e0f56fa76ef979ece1abbd2e809ede768
Fredrik Stroemberg <fredrik314@gmail.com>
2013-09-11 23:48:18 +0100

    fixing...

--- db_test.py
+++ db_test.py
@@ -501,7 +501,7 @@ class WDBtoMongo(WDBtoMFDB):
         for db in db_list:
             try: 
                 mtmp = db.number_of_known_factors(N,k,i)
-                print "mtmp1=",mtmp
+                print "db=",db
             except OSError:
                 mtmp = 0
             if mtmp>m:
