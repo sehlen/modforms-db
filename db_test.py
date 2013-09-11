8fed2780ac823605aeb5bf09a4e303d46e70adf5
Fredrik Stroemberg <fredrik314@gmail.com>
2013-09-11 17:26:26 +0100

    fixing...

--- db_test.py
+++ db_test.py
@@ -263,11 +263,11 @@ class WDBtoMongo(WDBtoMFDB):
         
     def show_existing_mongo(self,db='fr'):
         if db=='fr':
-            files = self._ms_mongod_fr['Modular_symbols'].files
-            factors = self._ms_mongod_fr['Newform_factors'].files
+            files = self._mongod_fr['Modular_symbols'].files
+            factors = self._mongod_fr['Newform_factors'].files
         else:
-            files = self._ms_mongod_to['Modular_symbols'].files
-            factors = self._ms_mongod_to['Newform_factors'].files
+            files = self._mongod_to['Modular_symbols'].files
+            factors = self._mongod_to['Newform_factors'].files
         levels = files.distinct('N')
         weights = files.distinct('k')
         print "files=",files
