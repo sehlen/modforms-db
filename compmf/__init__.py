ddb25172d9d943f6ea1794582706424d5b7d6e5f
Fredrik Stromberg <fredrik314@gmail.com>
2014-06-24 18:03:06 +0100

    Fixed imports and construction of logger when colorlog is not avialable

--- compmf/__init__.py
+++ compmf/__init__.py
@@ -1,20 +1,21 @@
 import logging
+LOG_LEVEL = logging.DEBUG
+logging.root.setLevel(LOG_LEVEL)
+LOGFORMAT = "  %(log_color)s%(levelname)-10s%(filename)s:%(lineno)d%(reset)s | %(log_color)s%(message)s%(reset)s"
+
 try:
     import colorlog
     from colorlog import ColoredFormatter
-    LOG_LEVEL = logging.DEBUG
-    logging.root.setLevel(LOG_LEVEL)
-    LOGFORMAT = "  %(log_color)s%(levelname)-10s%(filename)s:%(lineno)d%(reset)s | %(log_color)s%(message)s%(reset)s"
     formatter = ColoredFormatter(LOGFORMAT)
-    stream = logging.StreamHandler()
-    stream.setLevel(LOG_LEVEL)
-    stream.setFormatter(formatter)
-    clogger = logging.getLogger(__name__)
-    if not clogger.handlers:
-        clogger.addHandler(stream)
 except:
-    clogger = logging.getLogger(__name__)
-
+    formatter = logging.format(LOGFORMAT)
+stream = logging.StreamHandler()
+stream.setLevel(LOG_LEVEL)
+stream.setFormatter(formatter)
+clogger = logging.getLogger(__name__)
+if not clogger.handlers:
+    clogger.addHandler(stream)
+    
 clogger.setLevel(logging.DEBUG)
 
 import character_conversions
