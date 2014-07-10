b043f975807242ba998c1ef2716800ec7c6c4d4a
Fredrik Stroemberg <fredrik314@gmail.com>
2014-07-10 14:14:51 +0100

    fix loading from file

--- compmf/filesdb.py
+++ compmf/filesdb.py
@@ -38,7 +38,7 @@ by William Stein.
 
 """
 import os
-from sage.all import cached_function,prime_range,DirichletGroup,dimension_new_cusp_forms,save,load,trivial_character,Sequence,ModularSymbols
+from sage.all import cached_function,prime_range,DirichletGroup,dimension_new_cusp_forms,save,load,trivial_character,Sequence,ModularSymbols,prime_pi
 
 import sqlite3
 from stat import S_ISDIR
