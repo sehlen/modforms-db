605e58bafdd4a1b001695b5492dc2c60ae15be81
Fredrik Stroemberg <fredrik314@gmail.com>
2014-09-05 14:36:37 +0200

    working on generating routines

--- wmf/generate_web_data.py
+++ wmf/generate_web_data.py
@@ -23,10 +23,12 @@ AUTHORS:
 """
 
 import pymongo
+
+from sage.all import parallel
 from wmf import wmf_logger,WebNewForm_computing,WebModFormSpace_computing
 from compmf import MongoMF
 
-def generate_web_modform_spaces(level_range=[],weight_range=[],ncpus=1,host='localhost',port=int(37010)):
+def generate_web_modform_spaces(level_range=[],weight_range=[],chi_range=[],ncpus=1,host='localhost',port=int(37010)):
     r"""
     Compute and insert objects of type WebModFormSpace with levels in the given range.
 
@@ -34,30 +36,33 @@ def generate_web_modform_spaces(level_range=[],weight_range=[],ncpus=1,host='loc
 
     """
     try: 
-        D  = MongoMF(host,port,db)
+        D  = MongoMF(host,port)
     except pymongo.errors.ConnectionFailure as e:
         raise ConnectionFailure,"Can not connect to the database and fetch aps and spaces etc. Error: {0}".format(e.message)
     args = []; s={}
     if level_range <> []:
         if len(level_range)==1:
-            s['N']=level_range[0]
+            s['N']=int(level_range[0])
         else:
-            s['N']={"$gt":int(level_range[0]-1),"$lt":int(level_range[-1])}
+            s['N']={"$gt":int(level_range[0]-1),"$lt":int(level_range[-1]+1)}
     if weight_range <> []:
         if len(weight_range)==1:
-            s['k']=weight_range[0]
+            s['k']=int(weight_range[0])
         else:
-            s['k']={"$gt":int(weight_range[0]-1),"$lt":int(weight_range[-1])}
+            s['k']={"$gt":int(weight_range[0]-1),"$lt":int(weight_range[-1]+1)}
     if chi_range <>[]:
         if len(chi_range)==1:
-            s['chi']=chi_range[0]
+            s['chi']=int(chi_range[0])
         else:
-            s['chi']={"$gt":int(chi_range[0]-1),"$lt":int(chi_range[-1])}
-    s['complete']=int(2)
+            s['chi']={"$gt":int(chi_range[0]-1),"$lt":int(chi_range[-1]+1)}
+    s['complete']=int(3)
     q = D._modular_symbols.find(s).sort([('N',pymongo.ASCENDING),('k',pymongo.ASCENDING)])
     for r in q:
         N = r['N']; k=r['k']; chi=r['cchi']
-        args.append(N,k,chi)
+        args.append((N,k,chi))
+    print "s=",s
+    print "args=",args
+    
     if ncpus>=32:
         generate_one_webmodform_space32(args)
     elif ncpus>=16:
@@ -67,7 +72,7 @@ def generate_web_modform_spaces(level_range=[],weight_range=[],ncpus=1,host='loc
     elif ncpus>=4:
         generate_one_webmodform_space4(args)
     else:
-        generate_one_webmodform_space1(args)
+        generate_one_webmodform_space1_par(args)
         
 @parallel(ncpus=32)
 def generate_one_webmodform_space32(level,weight,chi,**kwds):
@@ -81,11 +86,15 @@ def generate_one_webmodform_space8(level,weight,chi,**kwds):
 @parallel(ncpus=4)
 def generate_one_webmodform_space4(level,weight,chi,**kwds):
     return generate_one_webmodform_space1(level,weight,chi,**kwds)
-    
+@parallel(ncpus=1)
+def generate_one_webmodform_space1_par(level,weight,chi,**kwds):
+    return generate_one_webmodform_space1(level,weight,chi,**kwds)
 
+    
 def generate_one_webmodform_space1(level,weight,chi):    
     r"""
     Generates one modform space.
 
     """
+    print "generate:",level,weight,chi
     M = WebModFormSpace_computing(level,weight,chi)
