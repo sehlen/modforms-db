a26cc0a4af2f8c6150dca60d45c145d99442fe3d
Fredrik Stroemberg <fredrik314@gmail.com>
2014-09-05 12:37:54 +0100

    added import of generating web_modform_spaces

--- wmf/__init__.py
+++ wmf/__init__.py
@@ -3,3 +3,4 @@ wmf_logger = make_logger('wdb')
 from web_newforms_computing import WebNewForm_computing
 from web_modform_space_computing import WebModFormSpace_computing
 
+from generate_web_data import generate_web_modform_spaces
