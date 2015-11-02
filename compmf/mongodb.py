7b5c5f84affdfcbe83ba695cf98ee1965fcc3979
Fredrik Stromberg <fredrik314@gmail.com>
2015-11-02 15:51:22 +0000

    debugging

--- compmf/mongodb.py
+++ compmf/mongodb.py
@@ -1145,7 +1145,7 @@ class CompMF(MongoMF):
                     old_aps = fs_ap.find_one({'filename':fname1})
                     if not old_aps is None:
                         fs_ap.delete(old_aps['_id'])
-                    apid = fs_ap.update_one(dumps( (E,v)),filename=fname1,
+                    apid = fs_ap.put(dumps( (E,v)),filename=fname1,
                                      N=int(N),k=int(k),chi=int(sage_i[1]),cchi=int(ci),
                                      character_galois_orbit=orbit,
                                      conrey_galois_orbit_number=int(on[1]),
