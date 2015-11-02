65b797e71369a04bda6f37df45a235bb8609a03e
Fredrik Stromberg <fredrik314@gmail.com>
2015-11-02 14:50:39 +0000

    changing to Conrey characters in  compute

--- compmf/filesdb.py
+++ compmf/filesdb.py
@@ -700,7 +700,7 @@ class FilenamesMFDBLoading(FilenamesMFDB):
         """
         N = M.level()
         k = M.weight()
-        clogger.debug("Creating  {0},{1},{2},{3}".format(N,type(N),k,type(k)))
+        clogger.debug("Creating  {0},{1},{2},{3},{4},{5}".format(N,type(N),k,type(k),i,type(i)))
         fname = self.ambient(N, k, i, makedir=True)
         if self.path_exists(fname):
             #clogger.debug("%s already exists; not recreating"%fname)
