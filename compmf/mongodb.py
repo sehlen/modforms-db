80a5a82fcba7d13e91189e68d4ca6b50b768b9ef
Fredrik Stromberg <fredrik314@gmail.com>
2015-11-03 23:57:14 +0000

    debugging

--- compmf/mongodb.py
+++ compmf/mongodb.py
@@ -2206,6 +2206,9 @@ class CheckingDB(CompMF):
 
                     ##  We now check that E,v is consistent: i.e. E is non-zero E*v exists and that we have the correct number of primes.
                 clogger.debug("Check if E[0,0]==0")
+                clogger.debug("Check if E[0,0]={0}".format(E[0,0]))
+                clogger.debug("Check if lenv=={0}".format(len(v)))
+                clogger.debug("E.ncols={0}".format(E.ncols()))
                 if (not (E[0,0] is 0)) and len(v)==E.ncols() and  prec_in_db >= prec:
                     res['aps'] = True
             maxprec = max(precs)
