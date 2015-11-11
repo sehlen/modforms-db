fa58f2ef469eb0e959eedca1f6ba09a0fa47451d
Fredrik Stromberg <fredrik314@gmail.com>
2015-11-11 18:19:58 -0500

    fix orbit labels

--- wmf/generate_web_data.py
+++ wmf/generate_web_data.py
@@ -755,19 +755,19 @@ def fix_orbit_labels(D):
     for r in D._modular_symbols.find():
         space_orbit_label = r.get('space_orbit_label')
         print space_orbit_label
-        if not space_orbit_label is None:
-            l =space_orbit_label.split(".")
-            if  l[2].isdigit():
-                continue
-            new_label = "{0}.{1}.{2}".format(l[0],l[1],l[2][1])
-        else:
-            N = r.get('N'); k=r.get('k'); ci=r.get('cchi')
-            if N is None or k is none or ci is None:
-                wmf_logger.critical("r={0}".format(r))
-                D._modular_symbols.remove({'_id':r['_id']})
-                continue
-            on = conrey_character_number_to_conrey_galois_orbit_number(N,ci)[1]
-            new_label="{0}.{1}.{2}".format(N,k,on)
+        #if not space_orbit_label is None:
+        #    l =space_orbit_label.split(".")
+        #    if  l[2].isdigit():
+        #        continue
+        #    new_label = "{0}.{1}.{2}".format(l[0],l[1],l[2][1])
+        #else:
+        N = r.get('N'); k=r.get('k'); ci=r.get('cchi')
+        if N is None or k is none or ci is None:
+            wmf_logger.critical("r={0}".format(r))
+            D._modular_symbols.remove({'_id':r['_id']})
+            continue
+        on = conrey_character_number_to_conrey_galois_orbit_number(N,ci)[1]
+        new_label="{0}.{1}.{2}".format(N,k,on)
         D._modular_symbols.update({'_id':r['_id']},{"$set":{'space_orbit_label':new_label}})
 
 def fix_orbit_labels_2(D):
