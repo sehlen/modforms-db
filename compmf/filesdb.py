f58f233258f909dc804647b2b9ce75086b12408b
Fredrik Stromberg <fredrik314@gmail.com>
2015-11-20 16:34:20 +0000

    fixed bugs

--- compmf/filesdb.py
+++ compmf/filesdb.py
@@ -415,12 +415,13 @@ class FilenamesMFDB(Filenames):
                     # we have nothing i the files database and don't care
                     # whether it is simply not computed or the space is empty
                     print fileList
-                    if d == 0:
-                        # definitely no newforms
-                        yield (N,k,i,0,0)
-                    else:
-                        # we just don't know the newforms yet
-                        yield (N,k,i,-1,0)
+                    return
+                    #if d == 0:
+                    # definitely no newforms
+                    yield (N,k,i,0,0)
+                    #else:
+                    #    # we just don't know the newforms yet
+                    #    yield (N,k,i,-1,0)
                 else:
                     for n in newforms:
                         maxp = None
