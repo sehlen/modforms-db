301c59165a5a24fa90268edb8c899da20a40fb99
Fredrik Stromberg <fredrik314@gmail.com>
2015-11-20 22:16:30 +0000

    fixed the update_known files

--- wmf/generate_web_data.py
+++ wmf/generate_web_data.py
@@ -1052,7 +1052,7 @@ def fix_pprec_to_nmax(D):
             D.delete_from_mongo('ap',r['_id'])
         n = len(c)
         nmax = nth_prime(n+1)-1
-        minn = 0
+        nmin = 0
         wmf_logger.debug("Updating from pprec:{0} to nmanx:{1}".format(pprec,nmax))
         D._aps.update({'_id':r['_id']},{"$set":{'nmax':nmax,'nmin':nmin},"$unset":{'pprec':''}})
         
