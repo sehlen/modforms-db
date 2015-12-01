2a24a665b9d681c594cc63b5946cffce5df73ec8
Fredrik Stromberg <fredrik314@gmail.com>
2015-12-01 18:31:57 +0000

    updated fixing routine

--- wmf/generate_web_data.py
+++ wmf/generate_web_data.py
@@ -1107,7 +1107,7 @@ def check_coefficients_one_record(N,k,ci,d,maxn,datadir='/home/stromberg/data/mo
                 db.commit()
         s = {'N':int(N),'k':int(k),'ci':int(ci),'d':int(d),'maxn':int(maxn)}
         q = C.find(s)
-        clogger.debug("Updating record for s={0}. count={1}".format(s,q.count()))
+        wmf_logger.debug("Updating record for s={0}. count={1}".format(s,q.count()))
         if q.count()>0:
             new_rec = {"$set":{'checked':True,'pprec':[int(pprec[0]),int(pprec[1])]}}
             for r in q:
@@ -1115,7 +1115,7 @@ def check_coefficients_one_record(N,k,ci,d,maxn,datadir='/home/stromberg/data/mo
         else:
             new_rec = {'N':int(N),'k':int(k),'ci':int(ci),'d':int(d),'maxn':int(maxn),'pprec':[int(pprec[0]),int(pprec[1])],'checked':True} 
             fid = C.insert(new_rec)
-            clogger.debug("inserted new record: {0}".format(fid))
+            wmf_logger.debug("inserted new record: {0}".format(fid))
             #D._db.delete_file(apfile)
 
 
