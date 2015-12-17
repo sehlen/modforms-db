80a2b2d430356e566a889617be24d31beba44f71
Fredrik Stromberg <fredrik314@gmail.com>
2015-12-17 00:23:31 +0000

    rversed a faulty logic for the ramanujan

--- compmf/mongodb.py
+++ compmf/mongodb.py
@@ -2414,7 +2414,7 @@ class CheckingDB(CompMF):
                     E,v = loads(fs_ap.get(id).read())
                     #c = multiply_mat_vec(E,v)
                     #a2 = c[0] #.abs()/RR(2.0)**(RR(k-1)/RR(2))
-                    if self.check_ramanujan_bound_cp(E,v,2,k):
+                    if not self.check_ramanujan_bound_cp(E,v,2,k):
                         c = multiply_mat_vec(E,v)
                         a2 = c[0].abs()/RR(2.0)**(RR(k-1)/RR(2))
                         clogger.critical("a(2)={0} does not satisfy the Ramanujan bound".format(a2))
