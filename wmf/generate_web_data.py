898721dfe740a1932b2168917695dfc4ed91966a
Fredrik Stromberg <fredrik314@gmail.com>
2016-03-24 18:11:33 +0000

    added fixing function for incorrect cyclotomic field in eigenvlues

--- wmf/generate_web_data.py
+++ wmf/generate_web_data.py
@@ -1868,7 +1868,7 @@ def fix_dimension_data(D):
             
 def fix_problematic_eigenvalues(D):
     F = WebNewForm_computing(1,12,1,'a')
-    for x in D._mongodb['webeigenvalues.files']:
+    for x in D._mongodb['webeigenvalues.files'].find():
         fid= x['_id']
         label = x['hecke_orbit_label']
         r = loads(F.eigenvalues._files.get(fid).read())
