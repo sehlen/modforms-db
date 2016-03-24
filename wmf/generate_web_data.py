c20f29750790275921cd806c57761bdd5619af01
Fredrik Stromberg <fredrik314@gmail.com>
2016-03-24 12:13:53 +0000

    added parity computation to the dimension table updates in modular forms space

--- wmf/generate_web_data.py
+++ wmf/generate_web_data.py
@@ -979,7 +979,7 @@ def update_existing_database_of_dimensions(D,nrange=[1,500],krange=[2,20]):
     from sage.all import  Gamma1
     C = D._mongodb['dimension_table']
     #cw= D._mongodb['webmodformspace'].find({'space_orbit_label':space_orbit_label}).count()
-    cm= D._modular_symbols.find({'space_orbit_label':space_orbit_label,'complete':{"$gt":int(data_record_checked_and_complete-1)}}).count()
+    #cm= D._modular_symbols.find({'space_orbit_label':space_orbit_label,'complete':{"$gt":int(data_record_checked_and_complete-1)}}).count()
     for r in D._mongodb['webmodformspace'].find({'version':float(1.3)}):
         x = conrey_character_from_number(r['level'],r['character'])
         cm= D._modular_symbols.find({'space_orbit_label':r['space_orbit_label'],'complete':{"$gt":int(data_record_checked_and_complete-1)}}).count()
