b9f9ec3d1ae65c190d710c86a926f1d5489cd211
Stephan Ehlen <stephan.j.ehlen@gmail.com>
2016-05-09 17:55:21 -0700

    check

--- wmf/test_data.py
+++ wmf/test_data.py
@@ -150,6 +150,7 @@ def check_deligne_one_form(f):
             wmf_logger.critical("The aps in the coefficients are incorrect for {0}. We got c({1})/n^(k-1)/2)={2} Please check!".format(f.hecke_orbit_label,p,t))
 
             return False
+    print f.max_cn()
     if p >= previous_prime(f.max_cn()):
         return False
     return True
